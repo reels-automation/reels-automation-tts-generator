18343c96f251582ba434129df741a38527db93b8
darthpedroo <pedrovillarino06@gmail.com>
2024-12-10 15:45:48 -0300

    Added producer for subtitles

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import ast
 import shutil
+import logging
 from minio import Minio
 from minio.error import S3Error
 from tts.google_tts import GoogleTts
@@ -26,9 +27,11 @@ def main():
         auto_offset_reset = "latest"
     )
 
-    minio_client = Minio("172.23.0.2:9000",
+    minio_client = Minio(
+    "172.19.0.2:9000",
     access_key="AKIAIOSFODNN7EXAMPLE",
-    secret_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY", secure=False,
+    secret_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+    secure=False
 )
 
     with app_consumer.get_consumer() as consumer:
@@ -41,40 +44,45 @@ def main():
                 raise ValueError(msg.error())
             else:
                 print("Message value: ", msg.value())
-                key = msg.key().decode("utf8")
+                #key = msg.key().decode("utf8")
                 msg_value = msg.value()
-                offset = msg.offset()
+                #offset = msg.offset()
                 consumer.store_offsets(msg)
                 msg_value_json_response = ast.literal_eval(msg_value.decode("utf-8"))
                 script = msg_value_json_response["script"]
                 tema = msg_value_json_response["tema"]["tema"]
                 audio_name = google_tts.generate_tts(script,tema)
                 new_audio_path = move_file(audio_name)
-
                 bucket_name = "audios-tts"
                 destination_file = audio_name
-
+                """
                 found = minio_client.bucket_exists(bucket_name)
                 if not found:
+                    print("a")
                     minio_client.make_bucket(bucket_name)
                     print("Created Bucket", bucket_name)
                 else:
+                    print("b")
                     print(f"Bucket: {bucket_name} already exists")
-                
+                """
                 minio_client.fput_object(
                     bucket_name, destination_file, new_audio_path
                 )
-
                 print(
                     f"{new_audio_path}, succesfully uploaded as object: {destination_file}, to bucket: {bucket_name}"
                 )
-
                 os.remove(new_audio_path)
 
-
-
-
-
+                app_producer = Application(
+                    broker_address="localhost:9092", loglevel="DEBUG"
+                )
+                with app_producer.get_producer() as producer:                    
+                    producer.produce(
+                        topic="audio_subtitles",
+                        key = "Ai Subtitles",
+                        value = str({"status": "OK", "audio_name": destination_file, "bucket_name": bucket_name})
+                    )
+                logging.info("Produced. Sleeping..")
 
 
 if __name__ == "__main__":
